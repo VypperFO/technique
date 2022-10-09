f62ef1dd742b42ab0fcef00055cd46cefda7b4d1
Felix <happytech01@icloud.com>
2022-10-09 11:57:51 -0400

    atelier 2 prog

---
+++ Programmation/Programmation 03/Ateliers/Atelier 2/ArrayStack.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main (){
+  int nbEtages;
+  int index;
+  int nbMouv;
+
+  printf("Saissisez le nombre d'etages: ");
+  scanf("%d", &nbEtages);
+
+  nbMouv = index;
+  printf("Nombre de mouvements: %d", nbMouv);
+  return 0;
+}
--- Programmation/Programmation 03/Ateliers/Atelier 2/main.c
+++ Programmation/Programmation 03/Ateliers/Atelier 2/main.c
@@ -1,84 +1,68 @@
 #include <stdio.h>
+#include <stdlib.h>
+#define MAX 8
 
-struct StackArray
-{
-    int stackSize = 8;
-    int stack[stackSize];
-    int top = -1;
+int stack[MAX];
+int top;
 
-    int isEmpty()
-    {
-        if (top == -1)
-            return 1;
-        else
-            return 0;
+int isEmpty() {
+    if (top == -1)
+        return 1;
+    else
+        return 0;
+}
+
+int isFull() {
+    if (top == MAX)
+        return 1;
+    else
+        return 0;
+}
+
+int peek() {
+    return stack[top];
+}
+
+int pop() {
+    int data;
+
+    if (!isEmpty()) {
+        data = stack[top];
+        top = top - 1;
+        return data;
+    } else {
+        printf("Could not retrieve data, Stack is empty.\n");
     }
+}
 
-    int isFull()
-    {
-        if (top == stackSize)
-            return 1;
-        else
-            return 0;
+int push(int data) {
+    if (!isFull()) {
+        top = top + 1;
+        stack[top] = data;
+    } else {
+        printf("Could not insert data, Stack is full.\n");
     }
-
-    int peek()
-    {
-        return stack[top];
-    }
-
-    int pop()
-    {
-        int data;
-
-        if (!isEmpty())
-        {
-            data = stack[top];
-            top = top - 1;
-            return data;
-        }
-        else
-        {
-            printf("Could not retrieve data, Stack is empty.\n");
-        }
-    }
-
-    int push(int data)
-    {
-
-        if (!isFull())
-        {
-            top = top + 1;
-            stack[top] = data;
-        }
-        else
-        {
-            printf("Could not insert data, Stack is full.\n");
-        }
-    }
-};
+}
 
 int main()
 {
-    struct StackArray stackArray;
-
     // push items on to the stack
-    stackArray.push(3);
-    stackArray.push(5);
-    stackArray.push(7);
-    stackArray.push(17);
+    push(3);
+    push(5);
+    push(7);
+    push(17);
 
-    printf("Element at top of the stack: %d\n", stackArray.peek());
+    printf("Element at top of the stack: %d\n", peek());
     printf("Elements: \n");
 
     // print stack data
-    while (!stackArray.isEmpty())
+    while (!isEmpty())
     {
-        int data = stackArray.pop();
+        int data = pop();
         printf("%d\n", data);
     }
 
-    printf("Stack full: %s\n", stackArray.isFull() ? "true" : "false");
-    printf("Stack empty: %s\n", stackArray.isEmpty() ? "true" : "false");
+    printf("Stack full: %s\n", isFull() ? "true" : "false");
+    printf("Stack empty: %s\n", isEmpty() ? "true" : "false");
     return 0;
 }
