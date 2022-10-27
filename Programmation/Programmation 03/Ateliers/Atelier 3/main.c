a8f889f988a9704addc5465eda4e0f9fcc9ac594
VypperFO <felixlatulip@gmail.com>
2022-10-27 12:07:46 -0400

    prog 3

--- Programmation/Programmation 03/Ateliers/Atelier 3/main.c
+++ Programmation/Programmation 03/Ateliers/Atelier 3/main.c
@@ -5,62 +5,31 @@
 #include <stdlib.h>
 #include "MD5.h"
 
-void bruteforce()
+void bruteforce(char *messageSecret, int min, int max)
 {
-  char messageSecret[] = "bbc";
-  char messageUser[] = {'a', 'a', 'a', 0};
-  int isCracked = 0;
-
-  int hex = 16;
-  int dec = 10;
-  char hashdaon[255] = "7f138a09169b250e9dcb378140907378";
-  char hash[255] = "123abc";
-  char *end;
-  int hashDecInt;
-  char hashDecChar[255];
-
-  // Convert input hash hex to dec
-  hashDecInt = strtol(hash, &end, hex);    // Copy hex to dec into a int
-  sprintf(hashDecChar, "%ld", hashDecInt); // Copy said int into a char array
-
-  // printf("%s", hashDecChar);
-
-  MD5_CTX context;
-  MD5Init(&context);
-
-  // printf("Message: ");
-  char message[255];
-  // scanf("%s", message);
-  MD5Update(&context, (unsigned char *)message, (unsigned int)strlen(message));
-
-  printf("MD5: ");
-  unsigned char digest[16];
-  MD5Final(&context, digest);
-  // for (unsigned char i = 0; i < 16; i++)
-  // printf("%c", digest[i]);
-
-  MD5_CTX context2;
-  MD5Init(&context2);
-
-  MD5Update(&context2, (unsigned char *)hash, (unsigned int)strlen(hash));
-  unsigned char digest2[16];
-  MD5Final(&context2, digest2);
-  // for (unsigned char i = 0; i < 16; i++)
-  // printf("%02x", digest2[i]);
+  int len = max - min;
+  char messageUser[len];
 
   for (size_t i = 0; i < strlen(messageUser); i++)
   {
     for (size_t j = 0; j < 255; j++)
     {
+      MD5_CTX context;
+      MD5Init(&context);
+
+      char message[255];
+      MD5Update(&context, (unsigned char *)message, (unsigned int)strlen(message));
+
+      unsigned char digest[16];
+      MD5Final(&context, digest);
       if (strcmp(messageSecret, messageUser) != 0)
       {
         messageUser[i]++;
-        printf("%s\n", messageUser);
+        printf("something");
       }
       else
       {
         printf("trouver\n");
-        isCracked = 1;
       }
     }
   }
@@ -71,38 +40,39 @@ int main(int argc, char *argv[])
   int charMin = 1;
   int charMax = 10;
 
-  for (size_t i = 0; i < 5; i++)
-  {
-    if (strcmp(argv[i], "-l") == 0)
+  /*
+
+    for (size_t i = 0; i < 5; i++)
     {
-      charMin = (int)argv[i + 1];
-      printf("%d\n", charMin);
+      if (strcmp(argv[i], "-l") == 0)
+      {
+        charMin = (int)argv[i + 1];
+        printf("%d\n", charMin);
+      }
+      else if (strcmp(argv[i], "-h") == 0)
+      {
+        charMax = (int)argv[i + 1];
+        printf("%d\n", charMax);
+      }
+      else
+      {
+        // printf("%d", charMin);
+        // printf("%d", charMax);
+        // printf("wassup");
+      }
     }
-    else if (strcmp(argv[i], "-h") == 0)
+  */
+  if (argc > 2)
+  {
+    if (strcmp(argv[1], "-c") == 0)
     {
-      charMax = (int)argv[i + 1];
-      printf("%d\n", charMax);
+      bruteforce("12345", 1, 10);
     }
     else
     {
-      // printf("%d", charMin);
-      // printf("%d", charMax);
-      // printf("wassup");
+      printf("No arguments passed through command line.\n");
     }
   }
-  /**
 
-      if (argc > 2)
-      {
-        if (strcmp(argv[1], "-c") == 0)
-        {
-          // bruteforce();
-        }
-        else
-        {
-          printf("No arguments passed through command line.\n");
-        }
-      }
-  */
   return 0;
 }
