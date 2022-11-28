3b827eca287571539340457294d9b2791775b188
VypperFO <felixlatulip@gmail.com>
2022-11-27 21:07:06 -0500

    prog 3 tp 2 possibleDirection()

--- Programmation/Programmation 03/TP/TP-02 - Prog/MazeSolver.hpp
+++ Programmation/Programmation 03/TP/TP-02 - Prog/MazeSolver.hpp
@@ -5,6 +5,8 @@
 #include "Window.hpp"
 #include "Stack.hpp"
 #include "Position.hpp"
+#include <ctime>
+
 /// TODO: Inclusions.
 
 class MazeSolver : public Window
@@ -70,15 +72,56 @@ public:
 			possibleDirection(pos);
 			pos->dir[provenance] = false;
 			path->push(pos);
-			path->top()->dir[provenance] = false;
 		}
 		else if (nbDirection(path->top()) > 1)
 		{
 			cout << "<1" << endl;
+
+			int provenance = -1;
+			int empreuter = -1;
+
+			if (path->top()->dir[0])
+			{
+				y++;
+				provenance = 1;
+				empreuter = 0;
+			}
+			if (path->top()->dir[1])
+			{
+				y--;
+				provenance = 0;
+				empreuter = 1;
+			}
+			if (path->top()->dir[2])
+			{
+				x--;
+				provenance = 3;
+				empreuter = 2;
+			}
+			if (path->top()->dir[3])
+			{
+				x++;
+				provenance = 2;
+				empreuter = 3;
+			}
+
+			Position *pos = new Position(x, y);
+			possibleDirection(pos);
+			pos->dir[provenance] = false;
+			path->push(pos);
 		}
 		else if (nbDirection(path->top()) == 0 && maze->getSquare(path->top()->x, path->top()->y) != Square::EXIT)
 		{
 			path->pop();
+		}
+
+		else if (path->size() == 0)
+		{
+			cout << "Aucun chemin trouver" << endl;
+		}
+		else
+		{
+			cout << "Une erreur est survenue" << endl;
 		}
 	}
 
@@ -148,6 +191,26 @@ public:
 		}
 
 		return pos;
+	}
+
+	int randomDirection(Position *pos)
+	{
+		int index = 0;
+		bool found;
+
+		while (found != true)
+		{
+			srand(time(0));			 // une seule fois dans tout le programme
+			int random = rand() % 4; // i = 0..3;
+
+			if (pos->dir[random])
+			{
+				found = true;
+				index = random;
+			}
+		}
+
+		return index;
 	}
 };
 
--- Programmation/Programmation 03/TP/TP-02 - Prog/main.cpp
+++ Programmation/Programmation 03/TP/TP-02 - Prog/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	Application::getInstance()->addWindow(new MazeSolver(new Maze(2)));
+	Application::getInstance()->addWindow(new MazeSolver(new Maze(1)));
 	Application::getInstance()->start();
 
 	/*Position *pos01 = new Position(1, 4);
