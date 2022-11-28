6beedcf913c4c2325aae2404c28cb3ac9753e33b
VypperFO <felixlatulip@gmail.com>
2022-11-28 14:08:07 -0500

    tp fini 2

--- Programmation/Programmation 03/TP/TP-02 - Prog/main.cpp
+++ Programmation/Programmation 03/TP/TP-02 - Prog/main.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char *argv[])
 {
-	Application::getInstance()->addWindow(new MazeSolver(new Maze(2)));
+	Application::getInstance()->addWindow(new MazeSolver(new Maze(3)));
 	Application::getInstance()->start();
 
 	return 0;
